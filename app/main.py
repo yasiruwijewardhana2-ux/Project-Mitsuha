8338dc2fd51ca9b7e62b77619f32b85480760ab9
Yasiru Wijewardhana <yasiruwijewardhana2@gmail.com>
2026-07-17 22:45:41 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,25 @@
 import sys
+import logging
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import Qt
 from app.ui.window import MainWindow
 from app.robot.companion_core import CompanionCore
 
-def main():
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+    )
+    QApplication.setAttribute(Qt.AA_UseDesktopOpenGL)
+
     app = QApplication(sys.argv)
-    
-    # Core එක ඉන්නවා
+
+    # Core එක විතරයි දැන් තියෙන්නේ
     core = CompanionCore()
     
     # Window එකට core එක දෙනවා
     window = MainWindow(core)
     window.show()
-    
-    sys.exit(app.exec())
 
-if __name__ == "__main__":
-    main()
+    sys.exit(app.exec())
