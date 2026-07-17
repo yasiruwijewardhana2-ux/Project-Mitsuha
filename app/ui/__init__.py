1b5733764656bd3b86ef4d4d1c7fc6c9e87399bc
Yasiru Wijewardhana <yasiruwijewardhana2@gmail.com>
2026-07-17 22:49:34 +0530

    Update __init__.py

--- app/ui/__init__.py
+++ app/ui/__init__.py
@@ -1,5 +1,1 @@
-class MainWindow(QMainWindow):
-    def __init__(self, core): # මෙතනට core එක එනවා
-        super().__init__()
-        self.core = core  # මෙතනින් ඒක save කරගන්න
-        # ... අනිත් UI වැඩ ...
+from .window import MainWindow
