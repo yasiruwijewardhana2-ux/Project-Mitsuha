461bac90ffe6bf5a741a413061205b39e8056ada
Yasiru Wijewardhana <yasiruwijewardhana2@gmail.com>
2026-07-17 18:57:33 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,35 +1,24 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
-from PySide6.QtGui import QPainter, QColor
+import logging
+
+from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt
 
-
-class MitsuhaWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("Project Mitsuha")
-        self.setFixedSize(320, 240)
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-
-        # Background
-        painter.fillRect(self.rect(), QColor(15, 15, 15))
-
-        painter.setBrush(QColor(255, 255, 255))
-        painter.setPen(Qt.NoPen)
-
-        # Left Eye
-        painter.drawEllipse(70, 80, 70, 70)
-
-        # Right Eye
-        painter.drawEllipse(180, 80, 70, 70)
+from app.ui.window import MainWindow
 
 
-app = QApplication(sys.argv)
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
 
-window = MitsuhaWindow()
-window.show()
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+    )
 
-sys.exit(app.exec())
+    QApplication.setAttribute(Qt.AA_UseDesktopOpenGL)
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
